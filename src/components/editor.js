b012103114cea000d6ff4eab5714886a9c3c0818
shenyan <shenyan@intern.douban.com>
2016-12-11 20:46:07 +0800

    process with block

--- src/components/editor.js
+++ src/components/editor.js
@@ -1,6 +1,7 @@
 import { Component } from 'react'
 import {Editor, 
         EditorState, 
+        ContentState,
         RichUtils,
         getDefaultKeyBinding,
         KeyBindingUtil,
@@ -13,54 +14,106 @@ require('../css/editor.styl')
 class MyEditor extends Component {
   constructor(props) {
     super(props);
-    this.state = {editorState: EditorState.createEmpty()}
-    this.onChange = (editorState) => this.setState({editorState})
+    this.state = {
+      editorState: EditorState.createEmpty(),
+      saved: true
+    }
+    this.onChange = this.onChange.bind(this)
     this.handleKeyCommand = this.handleKeyCommand.bind(this)
     this._onBoldClick = this._onBoldClick.bind(this)
+    this.saveState = this.saveState.bind(this)
+    this.logState = this.logState.bind(this)
+    this.reverse = this.reverse.bind(this)
+    this.select = this.select.bind(this)
   }
 
   _onBoldClick() {
     console.log('bold')
     this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
   }
+  
+  reverse() {
+    let contentState = this.state.editorState.getCurrentContent()
+    const blockArray = contentState.getBlocksAsArray()
+    blockArray.reverse()
+    contentState = ContentState.createFromBlockArray(blockArray)
+    const editorState = EditorState.createWithContent(contentState)
+    this.setState({editorState})
+    console.log('Reversed')
+  }
+
+  onChange(editorState) {
+    this.setState({
+      editorState,
+      saved: false
+    })
+  }
+
+  logState() {
+    const contentState = this.state.editorState.getCurrentContent()
+    console.log(contentState.getBlocksAsArray())
+  }
+
+  select() {
+    const selection = this.state.editorState.getSelection()
+    const editorState = this.state.editorState
+    const newState = RichUtils.toggleBlockType(editorState, 'header-two')
+    this.setState({
+      editorState: newState
+    })
+    console.log(selection.getFocusKey(), RichUtils.getCurrentBlockType(editorState))
+  }
 
   handleKeyCommand(command) {
     const newState = RichUtils.handleKeyCommand(this.state.editorState, command)
-    if (newState) {
-      this.onChange(newState)
-      return 'handled'
-    }
+      if (newState) {
+        this.onChange(newState)
+          return 'handled'
+      }
     if (command === 'myeditor-save') {
-      const currState = this.state.editorState.getCurrentContent()
-      localStorage['contentState'] = JSON.stringify(convertToRaw(currState))
-      console.log(currState)
-      console.log('Save')
+      this.saveState()
     }
     return 'not-handled'
   }
 
+  saveState() {
+    const currState = this.state.editorState.getCurrentContent()
+    localStorage['contentState'] = JSON.stringify(convertToRaw(currState))
+    this.setState({
+      saved: true
+    })
+    console.log(convertToRaw(currState))
+    console.log('Save')
+  }
+
   componentDidMount() {
     const rawData = localStorage['contentState']
-    const data = rawData ? JSON.parse(rawData) : null
-    const state = data ? EditorState.createWithContent(convertFromRaw(data)) : EditorState.createEmpty()
-    this.setState({
-      editorState: state
-    })
+      const data = rawData ? JSON.parse(rawData) : null
+      const state = data ? EditorState.createWithContent(convertFromRaw(data)) : EditorState.createEmpty()
+      this.setState({
+        editorState: state
+      })
   }
 
   render() {
     const {editorState} = this.state
-    return (
-      <div className='gakki'>
-        <button onClick={this._onBoldClick}>Bold</button>
-        <Editor 
-          editorState={editorState} 
-          onChange={this.onChange} 
-          handleKeyCommand={this.handleKeyCommand}
-          keyBindingFn={myKeyBindingFn}
-        />
-        </div>
-    )
+      return (
+          <div className='gakki'>
+            <div className='toolbar'>
+              <button onClick={this._onBoldClick}>Bold</button>
+              <button onClick={this.logState}>Log State</button>
+              <button onClick={this.reverse}>Reverse</button>
+              <button onClick={this.select}>H2: toggle</button>
+              <label>{this.state.saved?'已保存':'未保存'}</label>
+            </div>
+            <Editor 
+              editorState={editorState} 
+              onChange={this.onChange} 
+              handleKeyCommand={this.handleKeyCommand}
+              keyBindingFn={myKeyBindingFn}
+            />
+          </div>
+          )
   }
 }
 
